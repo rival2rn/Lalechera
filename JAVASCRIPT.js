ab9bfa099a9f44e3f26f79935b6626b61caf436b
rivaldo <rivaldo@hotmail.com>
2021-03-07 23:18:13 -0400

    la pract4 hecha

--- JAVASCRIPT.js
+++ JAVASCRIPT.js
@@ -1,2 +1,21 @@
-a = prompt ("CUAL ES TU NOMBRE?");
-document.write(a+" BIENVENIDO AL CURSO JS");
+let numeroA = parseInt ( prompt ("ingrese un numero")); 
+let numeroB = parseInt (prompt("ingrese un numero"));
+let numeroC = parseInt (prompt("ingrese un numero"));
+
+if (numeroA == numeroB && numeroA == numeroC)
+{
+document.writeln ("LOS 3 NUMEROS SON IGUALES");
+}
+
+ if (numeroA > numeroB && numeroA > numeroC)
+    {
+        document.writeln("LOS NUMEROS SON DIFERENTES, EL MAYOR ES ESTE:" + numeroA)
+    }
+    else if (numeroB > numeroA && numeroB > numeroC)
+    {
+        document.writeln("LOS NUMEROS SON DIFERENTES, EL MAYOR ES ESTE:" + numeroB)
+    }
+    else 
+    {
+        document.writeln("LOS NUMEROS SON DIFERENTES, EL MAYOR ES ESTE:" + numeroC)
+    }
---
+++ pract4/jehova.js
@@ -0,0 +1,11 @@
+function pe(lateral) {
+    perimetro = lateral * 4;
+    document.writeln("el perimetro es" + perimetro);
+}
+function supe(bh) {
+   superflano = Math.pow(bh,2);
+   document.writeln("La superficie" + superflano);   
+}
+
+pe(5);
+supe(9);
